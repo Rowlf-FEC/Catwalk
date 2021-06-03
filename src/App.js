4c35d1510a7b80291398845684f7bb2b5370c49e
Jason Wesson <jsnwesson@gmail.com>
2021-06-03 16:13:54 -0500

    Insert and Import components to App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import ProductDetails from './components/ProductDetails';
+import QandA from './components/QandA';
+import RatingsReviews from './components/RatingsReviews';
+import RelatedItems from './components/RelatedItems';
 // Config is an object that has the cohort and token as keys.
 import config from'./config.js';
-// Import components up here,
-
-// render them in the App div down below
 
 function App() {
   return (
     <div className="App">
-        {/* Insert components here */}
+        <ProductDetails />
+        <QandA />
+        <RatingsReviews />
+        <RelatedItems />
     </div>
   );
 }
