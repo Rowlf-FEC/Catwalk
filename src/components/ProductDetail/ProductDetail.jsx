c313d30805abefc77d1a77d7936a3702523baa32
Jason Wesson <jsnwesson@gmail.com>
2021-06-04 17:39:56 -0500

    Produce starting template for Class component. Need to verify config filepath and eventually test axios request.

--- src/components/ProductDetail/ProductDetail.jsx
+++ src/components/ProductDetail/ProductDetail.jsx
@@ -1,1 +1,36 @@
 import React from 'react';
+import config from'../src/config.js'; //need to confirm that this is the filepath to get to config.js
+
+export default class ProductDetail extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      productId: props.productId || 27197, //assuming that App.jsx has a state with key productId, otherwise default to an id
+      productInfo: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-atx/products/${this.productId}`, {
+    headers: {
+      'Authorization': `${config.token}`;
+    }}
+      .then(result => {
+        this.setState({
+          productInfo: result.data
+        });
+      });
+  }
+  render() {
+    return(
+      console.log('hello, world!')
+    );
+  }
+}
+
+
+/* axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-atx/products/${this.productId}`, {
+  headers: {
+    'Authorization': `${config.token}`;
+  }}
+*/
