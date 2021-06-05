769db8735e9a6ba67594010c5cfa6da051f5ab87
Jason Wesson <jsnwesson@gmail.com>
2021-06-04 19:43:54 -0500

    Update ProductDetail.jsx

--- src/components/ProductDetail/ProductDetail.jsx
+++ src/components/ProductDetail/ProductDetail.jsx
@@ -1,16 +1,42 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import config from'../../config.js'; // need to confirm that this is the filepath to get to config.js
 
-function ProductDetail() {
-  return (
-    <div>
-      ProductDetail
-    </div>
-  );
+export default class ProductDetail extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      productId: props.productId || 27197, // assuming that App.jsx has a state with key productId, otherwise default to an id
+      productInfo: [],
+    };
+  }
+
+  componentDidMount() {
+    axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-atx/products/${this.productId}`, {
+    headers: {
+      'Authorization': `${config.token}`
+    }}
+      .then(result => {
+        this.setState({
+          productInfo: result.data,
+        });
+      })
+    )};
+
+  // eslint-disable-next-line class-methods-use-this
+  render() {
+    return (
+      <div>
+        <div>
+          Hello, World!
+        </div>
+      </div>
+    );
+  }
 }
 
-// ProductDetail.propTypes = {
 
-// }
-
-export default ProductDetail;
+/* axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-atx/products/${this.productId}`, {
+  headers: {
+    'Authorization': `${config.token}`
+  }}
+*/
