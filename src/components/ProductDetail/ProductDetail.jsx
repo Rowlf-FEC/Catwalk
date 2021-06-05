c2574984ae0af688377b986d16d88a1665095abe
IMayCode <imaycode@gmail.com>
2021-06-04 19:37:42 -0500

    Added linting config

--- src/components/ProductDetail/ProductDetail.jsx
+++ src/components/ProductDetail/ProductDetail.jsx
@@ -1,1 +1,16 @@
 import React from 'react';
+// import PropTypes from 'prop-types';
+
+function ProductDetail() {
+  return (
+    <div>
+      ProductDetail
+    </div>
+  );
+}
+
+// ProductDetail.propTypes = {
+
+// }
+
+export default ProductDetail;
