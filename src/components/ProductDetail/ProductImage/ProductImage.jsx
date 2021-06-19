b475f40e9188ebd325f72748a4929f21bc659c2d
Jason Wesson <jsnwesson@gmail.com>
2021-06-19 09:57:10 -0500

    removes magnifier prior to deployment, keeps CSS for modal

--- src/components/ProductDetail/ProductImage/ProductImage.jsx
+++ src/components/ProductDetail/ProductImage/ProductImage.jsx
@@ -3,13 +3,13 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Modal } from 'semantic-ui-react';
-import { Magnifier, GlassMagnifier } from 'react-image-magnifiers';
+// import { Magnifier, GlassMagnifier } from 'react-image-magnifiers';
 import withSlide from './Carousel';
 import './ProductImage.css';
 
 function ProductImage({ images }) {
   const [open, setOpen] = useState(false);
-  const [zoom, setZoom] = useState(false);
+  // const [zoom, setZoom] = useState(false);
   const [currentSlide, setCurrentSlide] = useState(0);
 
   let mainCarousel;
@@ -29,9 +29,9 @@ function ProductImage({ images }) {
 
   const modalData = {
     className: 'modalCarousel',
-    onClickItem: () => {
-      setZoom(() => true);
-    },
+    // onClickItem: () => {
+    //   setZoom(() => true);
+    // },
     onChange: (index) => { setCurrentSlide(() => index); },
     selectedItem: currentSlide,
     showIndicators: true,
@@ -47,14 +47,14 @@ function ProductImage({ images }) {
         id="modalCarousel"
         onClose={() => {
           setOpen(false);
-          setZoom(false);
+          // setZoom(false);
         }}
         open={open}
       >
         <Modal.Content>
-          {zoom ? (
+          {/* {zoom ? (
             <div>
-              <Magnifier
+              <GlassMagnifier
                 className="magnifierImg"
                 cursorStyle="crosshair"
                 imageSrc={images[currentSlide].url}
@@ -63,7 +63,8 @@ function ProductImage({ images }) {
                 onZoomEnd={() => setZoom(false)}
               />
             </div>
-          ) : withSlide(modalCarousel, modalData)}
+          ) : withSlide(modalCarousel, modalData)} */}
+          {withSlide(modalCarousel, modalData)}
         </Modal.Content>
       </Modal>
     </div>
