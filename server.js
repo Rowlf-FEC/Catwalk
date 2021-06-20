a127405d6fc61d101efa22d76343cdd88e04ceac
Jason Wesson <jsnwesson@gmail.com>
2021-06-20 18:53:49 -0500

    Change server's dirname to lead to 'build'

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 // app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.listen(PORT, (error) => {
   if (error) {
