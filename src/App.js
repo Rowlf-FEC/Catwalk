5a062c2a54807e3d6335e4858b839276cc8cfdc7
Jason Wesson <jsnwesson@gmail.com>
2021-06-21 00:51:36 -0500

    Change productId

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import outline from './assets/WigglyFrameCircle.svg';
 // import config from'./config.js';
 
 function App() {
-  const productId = 27257;
+  const productId = 27189;
   return (
     <div className="App">
       <div className="topBar">
